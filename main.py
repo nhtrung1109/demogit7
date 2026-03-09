88468c682fd961f02ac67075e862cdd8cf9cae38
nguyen huu trung <nhtrung1109@gmail.com>
2026-03-09 09:45:36 +0700

    activèuntion

---
+++ main.py
@@ -0,0 +1,7 @@
+#sum a-b
+def sum(a,b):    return a+b
+#subtract a-b
+def subtract(a,b):    return a-b
+if __name__ == "__main__":
+    print("sum of 5 and 3 is ",sum(5,3))
+    print("subtract of 5 and 3 is ",subtract(5,3))
